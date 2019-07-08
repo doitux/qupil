356389e35564573b4f99abde73e1f0f2081b6ed9
Felix Hammer <f.hammer@web.de>
2019-07-08 12:36:49 +0200

    release name fix for 1.4.1

--- src/qupil_defs.h
+++ src/qupil_defs.h
@@ -2,6 +2,6 @@
 #define QUPIL_DEFS_H
 
 #define THIS_VERSION_DB_DATA_STRUCTURE  3
-#define RELEASE_STRING                  "1.4"
+#define RELEASE_STRING                  "1.4.1"
 
 #endif // QUPIL_DEFS_H
